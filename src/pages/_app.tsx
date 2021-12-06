8c2b68b76a6d65c5e4278510a657b5b94080b363
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-06 23:26:12 +0600

    redux store created

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 import Header from '../components/Header';
+import store from '../store/store';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <Provider store={store}>
             <Header />
             <Component {...pageProps} />
-        </>
+        </Provider>
     );
 }
 
