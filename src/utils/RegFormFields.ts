e4a66e941f84c3a9a05adea59c343f470fed6647
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-06 23:19:39 +0600

    declared fields for registration form

---
+++ src/utils/RegFormFields.ts
@@ -0,0 +1,75 @@
+const fields = {
+    personalInfo: [
+        {
+            label: 'Name',
+            type: 'text',
+            name: 'name',
+            placeholder: 'Full Name',
+        },
+        {
+            label: 'Email',
+            type: 'text',
+            name: 'email',
+            placeholder: 'Enter your email address',
+        },
+        {
+            label: 'Phone',
+            type: 'text',
+            name: 'phone',
+            placeholder: 'Enter your phone number',
+        },
+        {
+            label: 'Address',
+            type: 'text',
+            name: 'address',
+            placeholder: 'Enter your address',
+        },
+        {
+            label: 'Role',
+            type: 'text',
+            name: 'role',
+            placeholder: 'Enter your role',
+        },
+        {
+            label: 'Password',
+            type: 'text',
+            name: 'password',
+            placeholder: 'Enter your password',
+        },
+    ],
+
+    orgInfo: [
+        {
+            label: 'Name',
+            type: 'text',
+            name: 'orgName',
+            placeholder: 'Oranization Name',
+        },
+        {
+            label: 'Website',
+            type: 'text',
+            name: 'website',
+            placeholder: 'Organization website url',
+        },
+        {
+            label: 'Country',
+            type: 'text',
+            name: 'country',
+            placeholder: 'Country',
+        },
+        {
+            label: 'Address',
+            type: 'text',
+            name: 'orgAddress',
+            placeholder: 'Organization address',
+        },
+        {
+            label: 'Description',
+            type: 'text',
+            name: 'description',
+            placeholder: 'Organization description',
+        },
+    ],
+};
+
+export default fields;
