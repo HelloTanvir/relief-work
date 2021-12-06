e80a826581ee3ce1cda69c7d3f7fe616a9d7236c
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-06 23:03:50 +0600

    declared fields for login form

---
+++ src/utils/LoginFormFields.ts
@@ -0,0 +1,16 @@
+const fields = [
+    {
+        label: 'Phone',
+        type: 'text',
+        name: 'phone',
+        placeholder: 'Enter your phone number',
+    },
+    {
+        label: 'Password',
+        type: 'text',
+        name: 'password',
+        placeholder: 'Enter your password',
+    },
+];
+
+export default fields;
