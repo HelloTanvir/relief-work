c627f1ec15d39878a157960c45e1b0dcde00ab1f
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-15 20:29:53 +0600

    sidebar design changed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,23 +1,37 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import { AiTwotoneProject } from 'react-icons/ai';
+import { BsArrowLeftRight } from 'react-icons/bs';
 import { VscOrganization } from 'react-icons/vsc';
-import { Menu, MenuItem, ProSidebar } from 'react-pro-sidebar';
+import { Menu, MenuItem, ProSidebar, SidebarHeader } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 
-const Sidebar = () => (
-    <div style={{ height: 'calc(100vh - 64px' }}>
-        <ProSidebar style={{ height: '100%' }} image="/dark-background.jpg">
-            <Menu iconShape="circle">
-                <Link href="/login" passHref>
-                    <MenuItem icon={<VscOrganization />}>Organizations</MenuItem>
-                </Link>
+const Sidebar = () => {
+    const [collapsed, setCollapsed] = useState(true);
 
-                <Link href="/login" passHref>
-                    <MenuItem icon={<AiTwotoneProject />}>Projects</MenuItem>
-                </Link>
-            </Menu>
-        </ProSidebar>
-    </div>
-);
+    return (
+        <div style={{ height: 'calc(100vh - 57px)' }} className="shadow border-r w-max">
+            <ProSidebar width={250} collapsed={collapsed}>
+                <SidebarHeader className="flex justify-end py-3 pr-5 bg-gray-200">
+                    <BsArrowLeftRight
+                        className="text-white rounded-full p-1 bg-gray-700 cursor-pointer hover:bg-gray-600"
+                        fontSize={22}
+                        onClick={() => setCollapsed((prev) => !prev)}
+                    />
+                </SidebarHeader>
+
+                <Menu iconShape="circle">
+                    <Link href="/login" passHref>
+                        <MenuItem icon={<VscOrganization />}>Organizations</MenuItem>
+                    </Link>
+
+                    <Link href="/login" passHref>
+                        <MenuItem icon={<AiTwotoneProject />}>Projects</MenuItem>
+                    </Link>
+                </Menu>
+            </ProSidebar>
+        </div>
+    );
+};
 
 export default Sidebar;
