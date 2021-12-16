e358f9b9cee9a5782180ba3f014019b0acaec465
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-16 23:44:48 +0600

    z-index of loader increased to show it on top of everything

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import CircleLoader from 'react-spinners/CircleLoader';
 
 const Loader = () => (
     <div
-        className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center"
+        className="fixed inset-0 z-50 flex items-center justify-center"
         style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
     >
         <CircleLoader color="#C8F051" loading size={50} />
