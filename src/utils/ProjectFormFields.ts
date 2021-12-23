6098bcd3bd630e10171f6da425badf37dd01c988
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-24 00:24:39 +0600

    input field created for project update

---
+++ src/utils/ProjectFormFields.ts
@@ -0,0 +1,58 @@
+const fields = [
+    {
+        label: 'Organization Name',
+        type: 'text',
+        name: 'orgName',
+        placeholder: 'Organization Name',
+    },
+    {
+        label: 'Title',
+        type: 'text',
+        name: 'title',
+        placeholder: 'Title',
+    },
+    {
+        label: 'Status',
+        type: 'text',
+        name: 'status',
+        placeholder: 'Status',
+    },
+    {
+        label: 'District',
+        type: 'text',
+        name: 'district',
+        placeholder: 'District',
+    },
+    {
+        label: 'Thana',
+        type: 'text',
+        name: 'thana',
+        placeholder: 'Thana',
+    },
+    {
+        label: 'Venue',
+        type: 'text',
+        name: 'venue',
+        placeholder: 'Venue',
+    },
+    {
+        label: 'Event Start Date',
+        type: 'text',
+        name: 'eventStartDate',
+        placeholder: 'Event Start Date',
+    },
+    {
+        label: 'Event End Date',
+        type: 'text',
+        name: 'eventEndDate',
+        placeholder: 'Event End Date',
+    },
+    {
+        label: 'Description',
+        type: 'textarea',
+        name: 'description',
+        placeholder: 'Description',
+    },
+];
+
+export default fields;
