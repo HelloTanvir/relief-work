f1714393f30e9659eb080023efec4b5a88adee6d
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-26 12:00:19 +0600

    schema for project create and update separated

--- src/utils/ProjectFormSchema.ts
+++ src/utils/ProjectFormSchema.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-const schema = yup.object().shape({
+export const schemaForUpdate = yup.object().shape({
     orgName: yup.string().required('Organization name is required'),
 
     status: yup.string().required('Project status is required'),
@@ -18,4 +18,18 @@ const schema = yup.object().shape({
     eventStartDate: yup.string().required('Event start date is required'),
 });
 
-export default schema;
+export const schemaForCreate = yup.object().shape({
+    orgName: yup.string().required('Organization name is required'),
+
+    title: yup.string().required('Project title is required'),
+
+    description: yup.string().required('Project description is required'),
+
+    district: yup.string().required('District is required'),
+
+    thana: yup.string().required('Thana is required'),
+
+    venue: yup.string().required('Venue is required'),
+
+    eventStartDate: yup.string().required('Event start date is required'),
+});
