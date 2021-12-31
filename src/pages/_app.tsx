59e9205c8167ddfb7d05ab6756422369ec8b1fbe
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-31 21:41:38 +0600

    loader added on top of every component, this will trigger on route change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,40 @@
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
+import { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from '../components/Loader';
 import store from '../store/store';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const start = () => {
+            setLoading(true);
+        };
+
+        const end = () => {
+            setLoading(false);
+        };
+
+        Router.events.on('routeChangeStart', start);
+        Router.events.on('routeChangeComplete', end);
+        Router.events.on('routeChangeError', end);
+
+        return () => {
+            Router.events.off('routeChangeStart', start);
+            Router.events.off('routeChangeComplete', end);
+            Router.events.off('routeChangeError', end);
+        };
+    }, []);
+
     return (
         <Provider store={store}>
             <ToastContainer />
-            <Component {...pageProps} />
+            {loading ? <Loader /> : <Component {...pageProps} />}
         </Provider>
     );
 }
