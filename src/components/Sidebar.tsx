10245ef0d827c7164cb4158036d49553d4562c3d
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-02-05 11:45:09 +0600

    link to beneficiaries list page added, decreased sidebar width

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { AiTwotoneProject } from 'react-icons/ai';
 import { BsArrowLeftRight } from 'react-icons/bs';
+import { MdOutlineFeaturedPlayList } from 'react-icons/md';
 import { VscOrganization } from 'react-icons/vsc';
 import { Menu, MenuItem, ProSidebar, SidebarHeader } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
@@ -11,7 +12,7 @@ const Sidebar = () => {
 
     return (
         <div className="border-r shadow w-max h-[calc(100vh-57px)]">
-            <ProSidebar width={250} collapsed={collapsed}>
+            <ProSidebar width={200} collapsed={collapsed}>
                 <SidebarHeader className="flex justify-end py-3 pr-5 bg-gray-200">
                     <BsArrowLeftRight
                         className="p-1 text-white bg-gray-700 rounded-full cursor-pointer hover:bg-gray-600"
@@ -27,6 +28,10 @@ const Sidebar = () => {
 
                     <Link href="/projects" passHref>
                         <MenuItem icon={<AiTwotoneProject />}>Projects</MenuItem>
+                    </Link>
+
+                    <Link href="/beneficiaries" passHref>
+                        <MenuItem icon={<MdOutlineFeaturedPlayList />}>Beneficiaries</MenuItem>
                     </Link>
                 </Menu>
             </ProSidebar>
